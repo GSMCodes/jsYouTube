b2a51e5d8f686de2eae7b428d9bf84de43312e3a
GSMCodes <gurvanshsingh348@gmail.com>
2024-07-15 08:49:30 +0000

    till 5th video

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jsYouTube
 code repo for chai and JS youtube series.
+
+
+Reasons not to use var: 
+
+block and functional Scope issues. 
--- basics/01.js
+++ basics/01.js
@@ -1,1 +1,19 @@
-console.log("hello")
+//console.log("hello")
+
+//                                                      let vs const vs var
+
+/*let abc = "Delhi"
+var def = 12345
+const ghi = true
+jkl = 7.77 // not an advisable method.
+let mno; */
+
+//console.table([abc, def, ghi, jkl, mno]) // will give output in tabular form.
+
+//                                                        ::::DATATYPES::::
+
+"use strict"; //treat all the JS code in ewer version of JS  NOT MANDATORY as it is automatically executed
+
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
+//null and undefined are also datatypes in JS along with symbols.
